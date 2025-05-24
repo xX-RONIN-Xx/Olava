131951d0e310e6e064708cf2982fd23502ca27e1
Karen Simari <simarikaren@gmail.com>
2025-05-24 11:01:16 -0300

    clase 2 - Simulacion de response

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@nestjs/common';
 
+export interface iTrack {
+  id: number,
+  title: string,
+  duration: number,
+  artist: string
+}
+
+export const tracks: iTrack[] = [ //no es lo de ideal, es solo de prueba
+  {
+    id: 1,
+    title: "cancion 1",
+    duration: 120,
+    artist: "interprete 1"
+  },
+   {
+    id: 2,
+    title: "cancion 2",
+    duration: 130,
+    artist: "interprete 2"
+  },
+   {
+    id: 3,
+    title: "cancion 2",
+    duration: 220,
+    artist: "interprete 3"
+  }
+]
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello mundo!';
+  getTracks(): iTrack[] {
+    return tracks;
   }
 }
